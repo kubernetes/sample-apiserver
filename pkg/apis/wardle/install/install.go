1033084ddb0052b525e2b2852503dd7da6e7aeb5
sdojjy <sdojjy@qq.com>
2019-08-19 14:33:48 +0800

    install whitelist api

--- pkg/apis/wardle/install/install.go
+++ pkg/apis/wardle/install/install.go
@@ -20,6 +20,9 @@ import (
 	"k8s.io/apimachinery/pkg/runtime"
 	utilruntime "k8s.io/apimachinery/pkg/util/runtime"
 	"k8s.io/sample-apiserver/pkg/apis/wardle"
+
+	"k8s.io/sample-apiserver/pkg/apis/wardle/v1"
+
 	"k8s.io/sample-apiserver/pkg/apis/wardle/v1alpha1"
 	"k8s.io/sample-apiserver/pkg/apis/wardle/v1beta1"
 )
@@ -29,5 +32,7 @@ func Install(scheme *runtime.Scheme) {
 	utilruntime.Must(wardle.AddToScheme(scheme))
 	utilruntime.Must(v1beta1.AddToScheme(scheme))
 	utilruntime.Must(v1alpha1.AddToScheme(scheme))
-	utilruntime.Must(scheme.SetVersionPriority(v1beta1.SchemeGroupVersion, v1alpha1.SchemeGroupVersion))
+	utilruntime.Must(v1.AddToScheme(scheme))
+	utilruntime.Must(scheme.SetVersionPriority(v1.SchemeGroupVersion,
+		v1beta1.SchemeGroupVersion, v1alpha1.SchemeGroupVersion))
 }
--- pkg/apis/wardle/v1/register.go
+++ pkg/apis/wardle/v1/register.go
@@ -25,7 +25,7 @@ import (
 const GroupName = "wardle.k8s.io"
 
 // SchemeGroupVersion is group version used to register these objects
-var SchemeGroupVersion = schema.GroupVersion{Group: GroupName, Version: "v1alpha1"}
+var SchemeGroupVersion = schema.GroupVersion{Group: GroupName, Version: "v1"}
 
 var (
 	// TODO: move SchemeBuilder with zz_generated.deepcopy.go to k8s.io/api.
@@ -44,7 +44,10 @@ func init() {
 
 // Adds the list of known types to the given scheme.
 func addKnownTypes(scheme *runtime.Scheme) error {
-	scheme.AddKnownTypes(SchemeGroupVersion)
+	scheme.AddKnownTypes(SchemeGroupVersion,
+		&Whitelist2{},
+		&Whitelist2List{},
+	)
 	metav1.AddToGroupVersion(scheme, SchemeGroupVersion)
 	return nil
 }
